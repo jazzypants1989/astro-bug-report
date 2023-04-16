cbc2081c471b511a9edc4c0013d83111abfaaa5b
Jesse Pence <jessepence@gmail.com>
2023-04-16 00:56:12 -0700

    ugh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+// import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify/functions";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solidJs(), mdx()],
+  output: "server",
+  adapter: netlify()
+});
+
+// tailwind({
+//     config: {
+//       applyBaseStyles: false
+//     }
+//   })
---
+++ src/components/SolidPicker.tsx
@@ -0,0 +1,50 @@
+import { createSignal, Show } from "solid-js"
+import Buttons from "./Buttons"
+
+let randomID = Math.random().toString(36).substring(2, 9)
+
+export default function SolidPicker(props: {
+  code: string
+  demo: string
+  id: string
+  children: any
+}) {
+  const [active, setActive] = createSignal("code")
+
+  setTimeout(() => {
+    setActive("demo")
+  }, 1000)
+
+  return (
+    <div
+      class="w-full flex flex-col items-center justify-center"
+      id={props.id || randomID}
+    >
+      <Buttons
+        id={props.id || randomID}
+        code={props.code}
+        setActive={setActive}
+        direction="up"
+      />
+      <div class="flex items-center justify-center w-full mx-auto">
+        <Show when={active() === "code"}>
+          <div class="max-w-full" innerHTML={props.code} />
+        </Show>
+        <Show when={active() === "demo"}>
+          <div class="w-full text-center" innerHTML={props.demo} />
+        </Show>
+        <Show when={active() === "play"}>
+          <div class="w-full text-center">{props.children}</div>
+        </Show>
+      </div>
+      <Buttons
+        id={props.id || randomID}
+        code={props.code}
+        setActive={setActive}
+        direction="down"
+      />
+
+      <span class="hidden">{props.children}</span>
+    </div>
+  )
+}
